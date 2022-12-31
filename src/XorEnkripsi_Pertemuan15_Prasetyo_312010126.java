db2e21a10fcf8cbd2c24e31e51f7c1959d4c0aa4
Prasetyo <80664580+YooDevelops@users.noreply.github.com>
2023-01-01 03:22:48 +0700

    Add files via upload
    Berikut tugas pertemuan 15 Mata Kuliah Kriptografi Menggunakan XOR untuk Enkripsi

---
+++ src/XorEnkripsi_Pertemuan15_Prasetyo_312010126.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+
+import java.util.Scanner;
+
+public class XorEnkripsi_Pertemuan15_Prasetyo_312010126 {
+        public static Scanner in;
+        public static void main(String[] args) {
+            in = new Scanner(System.in);
+            System.out.print("Masukan Pesan Yang Akan Di Enkripsi: ");
+            String msg = in.nextLine();
+
+            System.out.print("Masukan Kuncinya: ");
+            String key = in.nextLine();
+
+            String encrypHexa = "";
+            int keyItr = 0;
+            for (int i = 0; i < msg.length(); i++) {
+                // XOR Operation
+                int temp = msg.charAt(i) ^ key.charAt(keyItr);
+
+                encrypHexa += String.format("%02x", (byte)temp);
+                keyItr++;
+                if(keyItr >= key.length()){
+                    // once all of key's letters are used, repeat the key
+                    keyItr = 0;
+                }
+
+            }
+
+            System.out.println("Pesan Sudah terenkripsi: " + encrypHexa);
+        }
+    }
+
